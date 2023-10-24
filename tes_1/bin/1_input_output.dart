5f9c051314b2ad1e21b86baf27d314400a14059f
ikbal <yayangikbal99@gmail.comgit initgit add.git commit -mmanifeshgit remote add gitlabhttps://gitlab.com/ie-indonesia-development/telly-mobile.git>
2023-10-24 10:38:04 +0700

    update no 2

--- tes_1/bin/1_input_output.dart
+++ tes_1/bin/1_input_output.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
+  // string//
+// contoh 1
+String? input;
+input = stdin.readLineSync();
+print(input);
+
+// contoh 2
   // String? input;
   // input = stdin.readLineSync();
   // print(input);
 
-    String? input;
-  input = stdin.readLineSync();
-  print(input);
+
 }
--- tes_1/bin/2_Mathematical_Operators.dart
+++ tes_1/bin/2_Mathematical_Operators.dart
@@ -1,16 +1,52 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-int number = 7;
-int number2 = 7;
-int result = number--;
-int result2 = --number2;
+// contoh 1
+// int number = 7;
+// int number2 = 7;
+// int result = number--;
+// int result2 = --number2;
 
-print(result);
-print(result2);
-print(number);
-print(number2);
+// print(result);
+// print(result2);
+// print(number);
+// print(number2);
 
-number ~/= 3;
-print(number);
+// number ~/= 3;
+// print(number);
+
+// contoh2
+// int number = 5;
+// number++;
+// print(number);
+// contoh 3
+// int number = 7;
+// int number2 = 7;
+// int result = number++;
+// int result2 = ++number2;
+// print(result);
+// print(result2);
+// print(number);
+// print(number2);
+// contoh 3
+// int number = 7;
+// int number2 = 7;
+// int result = number--;
+// int result2 = --number2;
+// print(result);
+// print(result2);
+// print(number);
+// print(number2);
+// contoh 3
+// int number = 7;
+// int number2 = 7;
+// int result = number++;
+// int result2 = ++number2;
+// print(result);
+// print(result2);
+// print(number);
+// print(number2);
+// number = number * 10;
+// number += 10;
+// print(number);
 }
