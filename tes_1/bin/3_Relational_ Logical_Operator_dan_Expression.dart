987f4b65a8ad154de184a979338533dc61220d42
ikbal <yayangikbal99@gmail.comgit initgit add.git commit -mmanifeshgit remote add gitlabhttps://gitlab.com/ie-indonesia-development/telly-mobile.git>
2023-10-24 11:43:25 +0700

    contoh baru

--- tes_1/bin/3_Relational_ Logical_Operator_dan_Expression.dart
+++ tes_1/bin/3_Relational_ Logical_Operator_dan_Expression.dart
@@ -1,11 +1,65 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
+// operator
+  // contoh 1
 // bool result;
 // result = (9 != 9) || (2 < 5) && (9 > 10);
 // print(result);
 
+// contoh 1(2)
+// bool result;
+// result = 5 < 6;
+// print(result);
+
+// Contoh 1(3)
+// bool result;
+// result = 9 <= 9;
+// print(result);
+
+// contoh 1(4)
+// bool result;
+// result = 9 > 9;
+// print(result);
+
+// contoh 1(4)
+// bool result;
+// result = 9 != 9;
+// print(result);
+
+// contoh 1(5)
+// bool result;
+// result = 9 == 9;
+// print(result);
+
+// contoh 1(6)
+// bool result;
+// result = (9 != 9) || (2 < 5);
+// print(result);
+
+// contoh 1(7)
+// bool result;
+// result = (9 != 9) && (2 < 5);
+// print(result);
+
+// contoh 1(8)
+// bool result;
+// result = (9 == 9) && (2 < 5);
+// print(result);
+
+// contoh 1(9)
+// bool result;
+// result = (9 != 9) \\ (2 < 5);
+// print(result);
+
+
+
 // EXPRESSION
-int number = 5;
-print((number + 10) * 2);
+// expression sederhana
+// int number = 5;
+// print(number);
+
+// experession kompleks
+// int number = 5;
+// print((number + 10) * 2);
 }
