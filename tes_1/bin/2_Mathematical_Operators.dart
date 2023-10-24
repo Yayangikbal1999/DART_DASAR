f5c45bf7f7ef99c4d1c6f6f4ebf1c01295db181e
ikbal <yayangikbal99@gmail.comgit initgit add.git commit -mmanifeshgit remote add gitlabhttps://gitlab.com/ie-indonesia-development/telly-mobile.git>
2023-10-24 11:21:31 +0700

    update baru

--- tes_1/bin/2_Mathematical_Operators.dart
+++ tes_1/bin/2_Mathematical_Operators.dart
@@ -6,20 +6,34 @@ void main(List<String> arguments) {
 // int number2 = 7;
 // int result = number--;
 // int result2 = --number2;
-
 // print(result);
 // print(result2);
 // print(number);
 // print(number2);
-
 // number ~/= 3;
 // print(number);
 
-// contoh2
+// contoh2 (posfik)
 // int number = 5;
 // number++;
 // print(number);
-// contoh 3
+
+// contoh 2(2)(posfik)
+// int number = 5;
+// number++;
+// print(number);
+
+// contoh 2 (3)(prefik)
+// int number = 5;
+// --number;
+// print(number);
+
+// contoh 2 (4)(prefik)
+//// int number = 5;
+// ++number;
+// print(number); 
+
+// contoh 3 (posfik dan prefik)
 // int number = 7;
 // int number2 = 7;
 // int result = number++;
@@ -28,7 +42,8 @@ void main(List<String> arguments) {
 // print(result2);
 // print(number);
 // print(number2);
-// contoh 3
+
+// contoh 3(2)(posfik dan prefik)
 // int number = 7;
 // int number2 = 7;
 // int result = number--;
@@ -37,7 +52,8 @@ void main(List<String> arguments) {
 // print(result2);
 // print(number);
 // print(number2);
-// contoh 3
+
+// contoh 3(3)(posfik dan prefik di tambahkan penjumlahan)
 // int number = 7;
 // int number2 = 7;
 // int result = number++;
@@ -49,8 +65,24 @@ void main(List<String> arguments) {
 // number = number * 10;
 // number += 10;
 // print(number);
+
 // contoh 4
 // double number;
 // number = 5 + 2;
 // print(number);
+
+// contoh 5
+// double number;
+//  number = 5 / 2;
+// int number2 = 5 ~/ 3;
+// print(number2);
+
+// contoh 5(2)
+// double number;
+//  number = 5 / 2;
+// int number2 = 5 % 3;
+// print(number2);
+
+// contoh 6
+
 }
